435302a8798e63a2f891f2f0244c2e172db189b3
DenisVitor <denisvitor41@gmail.com>
2023-12-19 15:52:05 -0300

    c

--- src/modules/commentaries/entities/commentary.entity.ts
+++ src/modules/commentaries/entities/commentary.entity.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto';
+
 export class Commentary {
   readonly id: string;
   commentary: string;
--- src/modules/items/entities/item.entity.ts
+++ src/modules/items/entities/item.entity.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto';
+
 export class Item {
   readonly id: string;
   name: string;
